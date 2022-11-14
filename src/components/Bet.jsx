5a8619595c36b4d09d58df9ce3e11cc2b7b5faf3
calvarado <calvarado@upre.gob.bo>
2022-11-14 16:22:27 -0400

    al 70%

--- src/components/Bet.jsx
+++ src/components/Bet.jsx
@@ -57,11 +57,43 @@ const listarPartidos = async()=>{
 
   // let respi = resp.sort((a,b)=> new Date(a.fechaPartido).getTime() - new Date(b.fechaPartido).getTime());
   // let respi = alasql('select * from ? order by ')
-  console.log('partidos',part);
-  setPartidos(part);
+  console.log('partidos y apuestas', pivotPasado,pivotActivos);
+  setPartidos(pivotPasado);
+  setApuestas(pivotActivos);
+}
+
+const paraRevisar = (r,row) =>{
+console.log(r,row);
 }
 
 const colPartidos = [
+  {field: 'Acciones', headerName: 'Acciones', sortable: false, width:80,
+    renderCell: (params) => {
+      const onClick = (e) => {
+        // e.stopPropagation(); // don't select this row after clicking
+        const api = params.api;
+        const thisRow = {};
+        api.getAllColumns().filter((c) => c.field !== '__check__' && !!c)
+          .forEach((c) => (thisRow[c.field] = params.getValue(params.id, c.field)));
+        // return alert(JSON.stringify(thisRow, null, 4));
+        paraRevisar(thisRow,params.row)
+      };
+      if(params.row.activo ===0) return <IconButton onClick={onClick} disabled title='Ya jugado' color='secondary' size="large"><CheckCircleIcon/></IconButton>;
+      return <IconButton onClick={onClick} title='Apostar' color='primary' size="large"><PaidIcon/></IconButton>;
+    },
+  },
+  {field:'fechaPartidoStr',headerName:'Fecha Partido', width: 120,editable:false},
+  {field:'equipoA',headerName:'Equipo A', width: 150,editable:false},
+  {field:'golesA',headerName:'Goles A', width: 70,editable:true,type:'number',min:0,max:9},
+  {field:'betA',headerName:'Goles A', width: 70,editable:true,type:'number',min:0,max:9},
+  {field:'equipoB',headerName:'Equipo B', width: 150,editable:false},
+  {field:'golesB',headerName:'Goles B', width: 70,editable:true,type:'number'},
+  {field:'betB',headerName:'Goles B', width: 70,editable:true,type:'number'},
+  {field:'id',headerName:'ID'},
+  {field:'apuestaID',headerName:'apuestaID'},
+  {field:'activo',headerName:'Activo'},
+]
+const colApuestas = [
   {field: 'Acciones', headerName: 'Acciones', sortable: false, width:80,
     renderCell: (params) => {
       const onClick = (e) => {
@@ -79,7 +111,7 @@ const colPartidos = [
   },
   {field:'fechaPartidoStr',headerName:'Fecha Partido', width: 120,editable:false},
   {field:'equipoA',headerName:'Equipo A', width: 150,editable:false},
-  {field:'betA',headerName:'Goles A', width: 70,editable:true,type:'number',min:0,max:9},
+  {field:'betA',headerName:'Apuesta A', width: 70,editable:true,type:'number',min:0,max:9},
   {field:'equipoB',headerName:'Equipo B', width: 150,editable:false},
   {field:'betB',headerName:'Goles B', width: 70,editable:true,type:'number'},
   {field:'id',headerName:'ID'},
@@ -99,14 +131,24 @@ const colPartidos = [
     <>
       <Navbar/>
       <Box component='main' sx={{backgroundColor:'whitesmoke',height:'100vh',width:'100vw',display:'flex',justifyContent:'center',gap:2}} >
-        <Box component="ul" sx={{alignItems:'center',width:{xs:'100vw',md:700},mt:4}}>
-          
+        <Box sx={{ height: 450, width:{xs:'98vw',md:700},justifyContent:'center',mt:4 }}>
+          <Typography variant="h5" sx={{fontWeight:500}} color="primary" >Historial Apuestas</Typography>
+          <DataGrid
+            rows={partidos}
+            columns={colPartidos}
+            pageSize={10}
+            rowsPerPageOptions={[10]}
+            disableSelectionOnClick
+            experimentalFeatures={{ newEditingApi: true }}
+            columnVisibilityModel={{id:false,apuestaID:false,activo:false}}
+            sortModel={[{field:'fechaPartido'}]}
+          />
         </Box>
         <Box sx={{ height: 450, width:{xs:'98vw',md:700},justifyContent:'center',mt:4 }}>
           <Typography variant="h5" sx={{fontWeight:500}} color="primary" >Apuestas disponibles</Typography>
           <DataGrid
-            rows={partidos}
-            columns={colPartidos}
+            rows={apuestas}
+            columns={colApuestas}
             pageSize={10}
             rowsPerPageOptions={[10]}
             disableSelectionOnClick
