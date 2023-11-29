78c462e3f217c3a41d37fd0bd6d68c1f33c0a32b
Cristian Alvarado <alvarado@upre.gob.bo>
2023-11-29 10:56:04 -0400

    'rep'

--- src/connection/firebase.js
+++ src/connection/firebase.js
@@ -4,37 +4,10 @@ import { getFirestore,collection, addDoc,getDocs, getDoc, deleteDoc,doc, updateD
 import {createUserWithEmailAndPassword, getAuth} from 'firebase/auth'
 
 
-// TODO: Add SDKs for Firebase products that you want to use
-
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-
-// Your web app's Firebase configuration
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-const firebaseConfig = {
-
-  apiKey: "AIzaSyDw2yYJ_Lw4fJr9pmBBCdfZIMXacwyD624",
-
-  authDomain: "mundial22-8981f.firebaseapp.com",
-
-  projectId: "mundial22-8981f",
-
-  storageBucket: "mundial22-8981f.appspot.com",
-
-  messagingSenderId: "718337084302",
-
-  appId: "1:718337084302:web:4a5bd1d7fb185c2b5432df",
-
-  measurementId: "G-LZWX20EKP6"
-
-};
-
 
 // Initialize Firebase
 
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(process.env.REACT_APP_FIREBASE_CONFIG);
 const db = getFirestore(app);
 
 // const analytics = getAnalytics(app);
